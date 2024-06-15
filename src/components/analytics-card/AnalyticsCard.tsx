39ff684c1fe95c40a2502c87a5b8f29139b71834
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-15 23:33:41 +0100

    added analytics card

---
+++ src/components/analytics-card/AnalyticsCard.tsx
@@ -0,0 +1,24 @@
+import "./style.scss";
+
+type AnalyticCardProps = {
+  icon: string;
+  color: string;
+  count: string;
+  title: string;
+};
+
+function AnalyticsCard(props: AnalyticCardProps) {
+  return (
+    <div className="analytics__card">
+      <span style={{backgroundColor: props.color}}>
+        <img src={props.icon} alt={props.title} />
+      </span>
+      <div>
+        <p>{props.title}</p>
+        <h4>24,607</h4>
+      </div>
+    </div>
+  );
+}
+
+export default AnalyticsCard;
