6827a44d84b09816dc2e0eca0bb4ff95c567ca00
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-17 23:33:24 +0100

    fixed note item types

--- src/pages/recording-page/NoteItem.tsx
+++ src/pages/recording-page/NoteItem.tsx
@@ -1,4 +1,6 @@
-type NoteSection = {
+import Loader from "../../components/shared/loader/circle-loader/Loader";
+
+export type NoteSection = {
   key: string;
   title: string;
   text: string;
@@ -6,22 +8,29 @@ type NoteSection = {
 };
 
 type GeneratedNoteProps = {
-   note: NoteSection[]
+  note: NoteSection[];
 };
 
-function NoteItem(props:GeneratedNoteProps) {
-  return <div className="note__tab">
-   {props.note.map(item => (
-      <div className="note__section">
-         <h4 className="title">{item.title}</h4>
-         <div className="note__content">
-            {item.content.map(content => (
-               <p>- {content}</p>
+function NoteItem(props: GeneratedNoteProps) {
+  return (
+    <div className="note__tab">
+      {props.note.map((item) => (
+        <div className="note__section">
+          <h4 className="title">{item.title}</h4>
+          <div className="note__content">
+            {item.content.map((content) => (
+              <p>- {content}</p>
             ))}
-         </div>
-      </div>
-   ))}
-  </div>;
+          </div>
+        </div>
+      ))}
+      {!props.note && (
+        <div className="tab__loader">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default NoteItem;
