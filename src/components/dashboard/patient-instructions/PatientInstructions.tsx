5312e8302b2084a36808e3334ede1ddcc1ae90bb
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-18 23:50:03 +0100

    fixed

---
+++ src/components/dashboard/patient-instructions/PatientInstructions.tsx
@@ -0,0 +1,12 @@
+type PatientInstructionProp = {
+
+};
+
+function PatientInstructions(props: PatientInstructionProp) {
+  return (
+    <div className="instructions__tab">
+      
+    </div>
+  )
+}
+export default PatientInstructions
