a1dec637441e1fd5bd8223f29acbe905a60afb79
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-18 23:50:55 +0100

    fixed

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -10,7 +10,7 @@ function Overview() {
     <div className="dashboard__overview">
       <div className="welcome__info">
         <div>
-          <h4>Welcome back, {currentUser?.username}</h4>
+          <h4>Welcome back, Dr.{currentUser?.username}</h4>
           <p>
             This page gives a brief overview of your recent sessions,
             encounters, and interactions with your patients. It provides a
@@ -18,32 +18,32 @@ function Overview() {
             follow-up notes for each patient you've seen recently.
           </p>
         </div>
-        <img src="/" alt="" />
+        <img src="/icons/welcome-bar.svg" alt="" />
       </div>
       <div className="analytics__container">
         <AnalyticsCard
           icon="/icons/analytics/patient.svg"
-          color="#a8d9ff"
+          color="#c1e4ff"
           count="23,490"
           title="Patient encounter"
         />
          <AnalyticsCard
-          icon="/icons/analytics/patient.svg"
-          color="#a8d9ff"
+          icon="/icons/analytics/live.svg"
+          color="#ffb7b7"
           count="23,490"
-          title="Patient encounter"
+          title="Total Live session"
         />
          <AnalyticsCard
-          icon="/icons/analytics/patient.svg"
-          color="#a8d9ff"
+          icon="/icons/analytics/file.svg"
+          color="#c8fdcb"
           count="23,490"
-          title="Patient encounter"
+          title="Total File Uploaded"
         />
          <AnalyticsCard
-          icon="/icons/analytics/patient.svg"
-          color="#a8d9ff"
+          icon="/icons/analytics/calendar.svg"
+          color="#fccfb7"
           count="23,490"
-          title="Patient encounter"
+          title="Total Days"
         />
       </div>
       <SessionChart />
