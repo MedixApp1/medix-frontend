131a3f1ff9e769ead66057992e0929aa7ae3e5e7
Chucks1093 <aniokechukwudi7@gmail.com>
2024-06-18 23:50:35 +0100

    fixed

--- src/pages/recording-page/NoteItem.tsx
+++ src/pages/recording-page/NoteItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Loader from "../../components/shared/loader/circle-loader/Loader";
 
 export type NoteSection = {
@@ -8,13 +9,15 @@ export type NoteSection = {
 };
 
 type GeneratedNoteProps = {
-  note: NoteSection[];
+  note: NoteSection[] | undefined;
 };
 
 function NoteItem(props: GeneratedNoteProps) {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="note__tab">
-      {props.note.map((item) => (
+      {props.note?.map((item) => (
         <div className="note__section">
           <h4 className="title">{item.title}</h4>
           <div className="note__content">
